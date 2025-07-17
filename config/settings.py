4c5e63a135b17d808609397bd20ae94940c3121c
kittiwake <kittiwake@mail.ru>
2025-07-17 18:02:55 +0300

    вернулись на sqlite

--- config/settings.py
+++ config/settings.py
@@ -77,25 +77,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.rubywcgfzqrnfzzalpzh',
-        'PASSWORD': 'ojnmFkhbZpNEPtnh',
-        'HOST': 'aws-0-eu-central-2.pooler.supabase.com',
-        'PORT': '5432',
-        'OPTIONS': {
-            'sslmode': 'require',  # Обязательно для Supabase
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres.rubywcgfzqrnfzzalpzh',
+#         'PASSWORD': 'ojnmFkhbZpNEPtnh',
+#         'HOST': 'aws-0-eu-central-2.pooler.supabase.com',
+#         'PORT': '5432',
+#         'OPTIONS': {
+#             'sslmode': 'require',  # Обязательно для Supabase
+#         },
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
