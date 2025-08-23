2b583ebe859e9b28049976eca9c57550f164337c
kittiwake <kittiwake@mail.ru>
2025-08-24 00:03:28 +0300

    fix миксин для прав доступа переписан

--- myauth/mixins.py
+++ myauth/mixins.py
@@ -1,9 +1,8 @@
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import redirect
-from django.contrib.auth.mixins import LoginRequiredMixin
 from employees.models import EmployeeModel
 
-class PermissionRequiredMixin:
+class PermissionsByUserMixin:
+
     """Миксин для проверки разрешений через должность сотрудника"""
     permission_required = None
     
@@ -28,26 +27,5 @@ class PermissionRequiredMixin:
                 permission__code_name=self.permission_required
             ).exists()
             
-        except (EmployeeModel.DoesNotExist, AttributeError):
-            return False
-
-class RoleRequiredMixin:
-    """Миксин для проверки конкретной должности"""
-    role_required = None  # Название должности
-    
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return self.handle_no_permission()
-        
-        if self.role_required and not self.has_role(request.user):
-            raise PermissionDenied("У вас нет нужной должности для этого действия")
-        
-        return super().dispatch(request, *args, **kwargs)
-    
-    def has_role(self, user):
-        """Проверяет, есть ли у пользователя нужная должность"""
-        try:
-            employee = user.employee
-            return employee.role.title == self.role_required
         except (EmployeeModel.DoesNotExist, AttributeError):
             return False
