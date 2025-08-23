38e101347c8c0314a3feee18664d4333ed3a5946
kittiwake <kittiwake@mail.ru>
2025-08-23 21:37:21 +0300

    добавляет к разрешениям должности

--- myauth/admin.py
+++ myauth/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from .models import PermissionModel, RolePermissionModel
 
+
+class RoleInline(admin.TabularInline):
+    """Inline для разрешений роли"""
+    model = RolePermissionModel
+    extra = 1  # Количество пустых форм
+    verbose_name = "Должность"
+    verbose_name_plural = "Должности"
+    autocomplete_fields = ['role']  # Автодополнение для разрешений
+
 @admin.register(PermissionModel)
 class PermissionAdmin(admin.ModelAdmin):
     list_display = ['name', 'code_name', 'description']
     list_filter = ['code_name']
     search_fields = ['name', 'code_name']
     ordering = ['name']
+    inlines = [RoleInline]
 
 @admin.register(RolePermissionModel)
 class RolePermissionAdmin(admin.ModelAdmin):
