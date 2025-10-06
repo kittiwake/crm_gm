1edbd94ca06c784c2a736b8dd5a03c9f0e6f0295
kittiwake <kittiwake@mail.ru>
2025-10-06 12:27:29 +0300

    set

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
     'widget_tweaks',
     'employees',
     'company',
@@ -56,7 +55,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
